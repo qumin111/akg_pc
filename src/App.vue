3cb521d3d24afe0cff48dc8e8a48199d8b4eea02
qu min <qumin186@163.com>
2019-10-04 13:09:21 +0800

    'href_m'

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ export default {
     let flag = navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)
 
     if (flag) {
-      window.location.href = 'http://www.akgvc.com/';
+      window.location.href = 'http://m.akgvc.com/';
     }
   },
 }
